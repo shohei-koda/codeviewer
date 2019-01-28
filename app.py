4a9bd14fcce090577a908a719a66d0a94bc251a2
shohei.koda <shohei.koda@gooya.co.jp>
2019-01-29 05:12:53 +0900

    no message

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def pycheker():
         data = json.dumps(request.json)
         arrData = json.loads(data)
 
-        pprint.pprint(arrData['repository']
+        pprint.pprint(arrData['repository'])
             
         return 'git webhooks'
 
